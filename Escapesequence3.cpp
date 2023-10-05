a013f8630532921f413ed49df794254bc740b103
Catherine nagonda <146429335+169680@users.noreply.github.com>
2023-10-05 20:21:54 +0300

    t

---
+++ Escapesequence3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main (){
+
+std::cout<< "------------------------------\n";
+
+    // Create stick figures (man and woman)
+    std::cout << "\t   O   O\n"; // Heads
+    std::cout << "\t   |   |\n"; // Necks
+    std::cout << "\t  /|\\//|\\\\n"; // Arms
+    std::cout << "\t / \\/ \\\n"; // Bodies
+    std::cout << " \t Man Woman\n";
+
+    // Create a car
+    std::cout << "\t  ------\n";
+    std::cout << "\t /   |   |\\\n";
+    std::cout << "\t |-/--\\--/--\\n";
+
+
+    std::cout << "   /o\\  /o\\n";
+    return 0;
+}
+
+
