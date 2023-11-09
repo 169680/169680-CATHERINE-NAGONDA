aebc5f95f2bc84e3e084d9d547ffb718425f8c64
169680 <146429335+169680@users.noreply.github.com>
2023-11-09 06:43:47 -0800

    HHFT

---
+++ 169680-CATHERINE-NAGONDA-main/#include _iostream_.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout<<"welcome to the wonderful world of C++!"<<endl;
+    cout<<"This is first c++ output statement"<<endl;
+    cout<<"soon, i will learn how to input data into my program"<<endl;
+    
+    return 0;
+}
---
+++ 169680-CATHERINE-NAGONDA-main/Escapesequence1.cpp
@@ -0,0 +1,14 @@
+//code to output statements
+#include <iostream>
+using namespace std;
+//define a function
+int main(){
+    cout << "strathmore university vision \n";
+    cout << "..............................\n";
+    cout << "To become a leading out-come driven enterpreneurial\n";
+    cout <<"research university by translating our excellence into\n";
+    cout <<"major contribution to culture,economic well-being and\n";
+    cout <<"quality life";
+
+return 0;
+}
